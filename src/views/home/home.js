b036c85b72448436d51565aaca90bf63c07e6c5d
sujiahao <13087949+sujiahao112@user.noreply.gitee.com>
2023-06-23 12:22:03 +0000

    !2 完成首页页面 * 完成首页功能

--- src/views/home/home.js
+++ src/views/home/home.js
@@ -1,10 +1,36 @@
 import a from './a';
 import './home.less';
-// import axios from 'axios';
-// axios
-//   .get('/bff/app-provision-api/default/banner', {
-//     params: {
-//       bannerType: 'tmall-banner',
-//     },
-//   })
-//   .then((res) => console.log(res));
+
+
+
+var swiper = new Swiper(".mySwiper", {
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+    loop: true,
+    //设置自动播放间隔时间
+    autoplay: {
+        delay: 2500,
+        disableOnInteraction: false,
+    },
+
+    on: {
+        slideChange: function () {
+            var realIndex = this.realIndex; //真实索引值
+        },
+    },
+});
+var swiper1 = new Swiper(".mySwiper1", {
+    slidesPerView: 3,
+    centeredSlides: true,
+    spaceBetween: 20,
+    pagination: {
+        el: ".swiper-pagination",
+        type: "fraction",
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+});
