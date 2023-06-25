a5f2f4005cd91258161a550eeb58f9b6d60ba27c
taFuture <3483988573@qq.com>
2023-06-25 12:05:31 +0000

    !15 修改门店尺寸大小 * fix: 修改门店尺寸大小

--- src/views/account/account.js
+++ src/views/account/account.js
@@ -33,16 +33,6 @@ eyes.addEventListener('click',function() {
         ones[1].type = 'password';
     }
 })
-// eyes.addEventListener('click',function() {
-//     sum++;
-//     console.log(111);
-//     if(sum%2 == 1) {
-//         eyes.style.background = `url(https://www-static.chinacdn.starbucks.com.cn/prod/assets/icons/icon-show-password.svg)`
-//     }else {
-//         eyes.style.background = `url(https://www-static.chinacdn.starbucks.com.cn/prod/assets/icons/icon-hide-password.svg
-//         )`
-//     }
-// })
 
 // 勾选
 let pNode = document.querySelector('.next>p');
@@ -54,5 +44,54 @@ next.addEventListener('click',function() {
     }else {
         pNode.classList.remove('green');
     }
+})
+
+// 滑块验证
+let blue = document.getElementById('blue');// 滑块的节点
+let bgSmall = document.getElementById('bgSmall');// 小插块的图片
+console.log(blue,bgSmall);
+let initialX,initialY; // 储存初始触摸位置
+let offsetX = 0,offsetY = 0; // 设置一开始的偏移量为0
+
+// 获取元素的起始位置
+let initialPosition = blue.getBoundingClientRect();
+console.log(initialPosition.left);
+
+// 监听触摸开始事件
+blue.addEventListener('touchstart',function(e) {
+    let touch = e.touches[0];//获取第一个触摸点的信息
+
+    // 记录第一个出点的初始位置
+    initialX = touch.clientX;
+    initialY = touch.clientY;
+
+    offsetX = parseFloat(blue.style.left) || 0
+    offsetY = parseFloat(blue.style.top) || 0
+});
+
+// 监听触摸移动事件
+blue.addEventListener('touchmove',function(e) {
+    // 阻止默认事件
+    e.preventDefault();
+
+    let touch = e.touches[0];
+
+    // 计算触摸位与初始位置的差值
+    let moveX = touch.clientX - initialX;
+
+    // 计算元素的新位置
+    let newPosition = offsetX + moveX;
+
+    // 判断新位置是否超出起始位置
+    if(newPosition < initialPosition.left) {
+        blue.style.left = initialPosition.left + 'px';
+    }
+})
+
+// 监听触摸结束事件
+blue.addEventListener('touchend',function() {
+    // 清空初始位置的变量
+    initialX = null
+    initialY = null
 })
 
